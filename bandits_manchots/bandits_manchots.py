50c03d129d858ad1726d195ad44c3a54fbdfc155
Haya Mamlouk <hayamamlouk25@gmail.com>
2023-10-02 22:54:30 +0200

    adapt espilon greedy and ucb functions for code logic

--- bandits_manchots/bandits_manchots.py
+++ bandits_manchots/bandits_manchots.py
@@ -1,12 +1,15 @@
 import random 
 import numpy as np
+from math import sqrt, log
 
 # Variables globales
 N = 10
-T = 1000
+T = 3000
 T_greedy = 400
 greedy_call_count = 0
 greedy_max_ind = 0
+ucb_call_count = 0
+T_ucb = 1000
 
 
 # Liste des rendements 
@@ -42,6 +45,32 @@ def greedy(recomp_estimees, count, count_victory):
         return aleatoire(recomp_estimees, count, count_victory)
     return greedy_max_ind
 
+def epsilon_greedy(recomp_estimees, count, count_victory):
+    #note: un epsilon élevé encourage à plus d'exploration, un epsilon faible encourage à plus d'exploitation
+    epsilon = random.uniform(0,1) #probabilité de choisir greedy
+    a = 0 
+    #si un nombre aléatoire < epsilon on applique aleatoire, sinon on applique greedy
+    if random.uniform(0,1) < epsilon:
+        a = aleatoire(recomp_estimees, count, count_victory)
+    else:
+         a = greedy(recomp_estimees, count, count_victory)
+
+    return a
+
+def UCB(recomp_estimees, count, count_victory):
+    global ucb_call_count 
+
+    if ucb_call_count < T_ucb :  #jouer qq coups pour ne pas avoir des valeurs nulles
+        ucb_call_count += 1
+        return aleatoire(recomp_estimees, count, count_victory)
+    
+    mû =  [a / b if b != 0 else 0 for a, b in zip(count_victory, count)] # les recompenses estimés
+    t = sum(count) # l'instant t est le i-eme coup joué, donc la somme des coups  jusqu'a présentn == le nombre total d'itérations jusqu'à présent.
+
+    ucb_val = [mû[i] + sqrt((2 * log(t + 1) / count[i])) for i in range(N)] # on comstruit la liste de la somme de la recompenses estimée (mû) et la racine carré du log...
+    a = ucb_val.index(max(ucb_val)) #on renvoie le numéro du levier avec la valeur maximale
+
+    return a
 
 def simulate(recomp_estimees, count, count_victory, strategie, T):
    
@@ -51,18 +80,20 @@ def simulate(recomp_estimees, count, count_victory, strategie, T):
         if tirage_bernouilli(rendements, a):
             count_victory[a] += 1
 
-        if i == T_greedy :
+        if i == T_greedy :  #to delete once algo is clear 
             print("count_victory with  i = T_greedy : ")
             print(count_victory)
 
             print("count i = T_greedy : ")
             print(count) 
         
-    if strategie == greedy :
-        global greedy_call_count
-        global greedy_max_ind
-        greedy_call_count = 0 
-        greedy_max_ind = 0
+   #reset all global variables
+    global greedy_call_count
+    global greedy_max_ind
+    global ucb_call_count
+    greedy_call_count = 0 
+    greedy_max_ind = 0
+    ucb_call_count = 0
 
     recomp_estimees = [a / b if b != 0 else 0 for a, b in zip(count_victory, count)]
     return recomp_estimees, count, count_victory
@@ -71,7 +102,7 @@ def simulate(recomp_estimees, count, count_victory, strategie, T):
 count_victory = [0] * N
 recomp_estimees = [0] * N
 count = [0] * N
-recomp_estimees, count, count_victory = simulate(recomp_estimees, count, count_victory, greedy, T)
+recomp_estimees, count, count_victory = simulate(recomp_estimees, count, count_victory, UCB, T)
 
 print("count_victory result : ")
 print(count_victory)
