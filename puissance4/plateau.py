32669fd5c3eba1943b5f99b8f034f4bfdeeb17e4
Haya Mamlouk <hayamamlouk25@gmail.com>
2023-10-11 22:46:44 +0200

    plateau.py propre

--- puissance4/plateau.py
+++ puissance4/plateau.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-from matplotlib import pyplot
 from parametres import *
 
 class Plateau: 
@@ -9,35 +8,44 @@ class Plateau:
     """
 
     #Attributs statiques
-    w_combos = quad_list(NB_LIGNES, NB_COLONNES, PUISSANCE)
-
+    w_combos = quad_list(NB_LIGNES, NB_COLONNES, PUISSANCE) #liste des quadruplets gagnants
     def __init__(self, nb_colonnes, nb_lignes):
         #Attributs de classe
         self.plateau = np.zeros((nb_lignes, nb_colonnes), dtype=int)
-        self.filled_cases = np.zeros(NB_COLONNES, dtype=int)
+        self.filled_cases = np.zeros(NB_COLONNES, dtype=int) #nb de lignes occupées par colonne
         self.nb_colonnes = nb_colonnes
         self.nb_lignes = nb_lignes
         self.running = True 
         self.tour = 1
         self.end = 0
-        self.possible_actions = [0,1,2,3,4,5,6]
+        self.possible_actions = [0,1,2,3,4,5,6] #colonne qu'on peut jouer
         self.nb_coups = 0
     
     def reset(self):
+        """
+        Réinitialise le plateau
+        """
         self.plateau = np.zeros((NB_LIGNES, NB_COLONNES) , dtype = int)
-    
+  
     def show(self):
-        """ Affiche le tableau de dimensions nb_lignes  x nb_colonnes"""
+        """ 
+        Affiche le plateau de dimensions nb_lignes  x nb_colonnes
+        """
         data = self.plateau
         df = pd.DataFrame(data)
         print("Voici un visuel du plateau")
         print(df)
 
     def is_full(self) :
-        """"Renvoie True si le plateau est complet, False sinon"""
+        """"
+        Renvoie True si le plateau est complet, False sinon
+        """
         return np.all(self.plateau != 0)
     
     def play(self, x, joueur):
+        """
+        Permet au joueur de placer un jeton dans la colonne x
+        """
         #Cette ligne permet de placer le jeton sur la première ligne libre (filled_cases étant les lignes occupées)
         y = (NB_LIGNES - 1) - self.filled_cases[x]
         self.filled_cases[x] += 1
@@ -48,14 +56,19 @@ class Plateau:
             self.possible_actions.remove(x)
  
     def has_won(self):
+        """
+        Renvoie (True,1) si le joueur 1 a gagné, (True,-1) si le joueur 2 a gagné, (False, 0) sinon.
+        """
         combo = []
         for quad in self.w_combos:
-            combo = [self.plateau[x][y] for x,y in quad]
+            combo = [self.plateau[x][y] for x,y in quad] #recupere l'indice (1, -1 ou 0) des cases pour l'ensemble des quadruplets gagnants
+
+            #Verifie si le joueur 1 a aligné 4 jetons
             if all(item == ID_JOUEUR1 for item in combo) :
-                #print("Le Joueur 1 a gagné")
                 return (True, 1)
+            
+            #Verifie si le joueur 2 a aligné 4 jetons
             if all(item == ID_JOUEUR2 for item in combo):
-                #print("Le Joueur 2 a gagné")
                 return (True, -1)
         return (False, 0)
     
@@ -63,8 +76,8 @@ class Plateau:
         """
         Permet de jouer une partie entre le joueur 1 et le joueur 2 : ils jouent à tour de rôle tant que la partie n'est pas finie.
         Elle renvoie 1 ou -1 selon la victoire du joueur 1 ou 2, et 0 en cas de nul.
-        Si mode vaut 0, l'utilisateur saisie le colonne souhaitée du prochain coup via la ligne de commande
-        Si mode vaut 1, on génere une colonne aléatoire
+        Si mode vaut 0, on génere une colonne aléatoire
+        Si mode vaut 1, on utilise l'algorithme de Monte-carlo pour choisir une colonne
         """
         while self.running :
             if self.tour == 1 :
@@ -82,13 +95,16 @@ class Plateau:
         return(self.end, self.nb_coups)
 
     def is_finished(self) :
+        """
+        Renvoie True si un des deux joueurs a gagné ou si le plateau est complet et termine la partie en cours
+        """
         has_won = self.has_won()
-        if has_won[0] :
+        if has_won[0] :      #Vérifie si un joueur a gagné
             self.running = False
             self.end = has_won[1]
             return True
-        if self.is_full() :
-            #print("Le plateau est complet ! ")
+        
+        if self.is_full() :   #Vérifie si le plateau est complet
             self.running = False
             return True
 
